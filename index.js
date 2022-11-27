0473acdb9cebd78f637657f3a3f471b0b0322ac3
LittleLebowski <o.f.atas@gmail.com>
2022-11-27 19:03:31 +0300

    assinment complete

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+const url = "https://anapioficeandfire.com/api/books";
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const x = fetch(url)
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
+  return x;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
